bdb193fa620caf235dab836d869537cc3944e132
Ivo de Kler <ivo.de.kler@gmail.com>
2014-03-20 15:00:35 +0100

    added zip validation regexes

--- formats.js
+++ formats.js
@@ -1,3 +1,12 @@
+var toImplement = {
+	zipValidation: {
+		de: /^\d{2}\s?\d{3}$/, // 2 digits + [space] + 3 digits, e.g., '01000' or '01 000'
+		us: /^\d{5}(-\d{4})?$/, // 5 digits  + ['-' + 4 digits], e.g., '12345-0011' or '12345')
+		br: /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
+		/*( ^ this validation was taken from http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive#answer-7259020 )*/
+	}
+}
+
 module.exports = {
 	defaults: [
 		[{
@@ -84,7 +93,8 @@ module.exports = {
 		}],
 		[{
 			name: 'zip',
-			example: '1017HG'
+			example: '1017HG',
+			validator: /^\d{4}\s?\w{2}$/ // 4 digits + [space] + 2 letters, e.g., '1000 PK' or '1000PK' or '1000pk'
 		}]
 	],
 	uk: [
