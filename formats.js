1cfbc8ccc051e7e8204fd5431a99b88b586c323e
Paul Wijhenke <paul@peerby.com>
2014-03-23 17:55:33 +0000

    New API format and tests (as a starting point)

--- formats.js
+++ formats.js
@@ -1,112 +1,124 @@
-var toImplement = {
-	zipValidation: {
-		de: /^\d{2}\s?\d{3}$/, // 2 digits + [space] + 3 digits, e.g., '01000' or '01 000'
-		us: /^\d{5}(-\d{4})?$/, // 5 digits  + ['-' + 4 digits], e.g., '12345-0011' or '12345')
-		br: /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
-		/*( ^ this validation was taken from http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive#answer-7259020 )*/
-	}
-}
+var _ = require('underscore');
+
+
+/*
+    Not implemented yet, borrowed from:
+        http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive#answer-7259020
+
+    de: /^\d{2}\s?\d{3}$/, // 2 digits + [space] + 3 digits, e.g., '01000' or '01 000'
+    us: /^\d{5}(-\d{4})?$/, // 5 digits  + ['-' + 4 digits], e.g., '12345-0011' or '12345')
+    br: /^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/
+*/
+
+
+var defaults = {
+    fields: {
+        addressLineOne: {},
+        addressLineTwo: {},
+        locality: {},
+        state: {},
+        zip: {}
+    },
+    presentation: [
+        ['addressLineOne'],
+        ['addressLineTwo'],
+        ['locality'],
+        ['state', 'zip']
+    ]
+};
+
+
+var nl = {
+    fields: {
+        streetName: {
+            example: 'Hoofdstraat'
+        },
+        streetNumber: {
+            example: '101C'
+        },
+        zip: {
+            example: '1017HG',
+            regexp: /^\d{4}\s?\w{2}$/
+        }
+    },
+    presentation: [
+        ['streetName', 'streetNumber'],
+        ['zip']
+    ]
+};
+
+
+var uk = {
+    fields: {
+        addressLineOne: {},
+        addressLineTwo: {
+            optional: true
+        },
+        zip: {}
+    },
+    presentation: [
+        ['addressLineOne'],
+        ['addressLineTwo'],
+        ['zip'],
+    ]
+};
+
+
+var ie = {
+    fields: {
+        addressLineOne: {},
+        addressLineTwo: {
+            optional: true
+        },
+        locality: {},
+        zip: {},
+        county: {
+            values: [
+                "Carlow",
+                "Cavan",
+                "Clare",
+                "Cork",
+                "Donegal",
+                "Dublin",
+                "Galway",
+                "Kerry",
+                "Kildare",
+                "Kilkenny",
+                "Laois",
+                "Leitrim",
+                "Limerick",
+                "Longford",
+                "Louth",
+                "Mayo",
+                "Meath",
+                "Monaghan",
+                "Offaly",
+                "Roscommon",
+                "Sligo",
+                "Tipperary",
+                "Waterford",
+                "Westmeath",
+                "Wexford",
+                "Wicklow"
+            ]
+        }
+    },
+    presentation: [
+        ['addressLineOne'],
+        ['addressLineTwo'],
+        ['zip'],
+    ]
+};
+
+
+/*
+    Exports
+*/
 
 module.exports = {
-	defaults: [
-		[{
-			name: 'addressLineOne'
-		}],
-		[{
-			name: 'addressLineTwo'
-		}],
-		[{
-			name: 'locality'
-		}],
-		[{
-			name: 'state'
-		},
-		{
-			name: 'zip'
-		}],
-	],
-	be: [
-		[{
-			name: 'streetName',
-			example: 'Hoofdstraat'
-		},
-		{
-			name: 'streetNumber',
-			example: '101C'
-		}],
-		[{
-			name: 'zip',
-			example: '1017HG'
-		}]
-	],
-	ie: [
-		[{
-			name: 'addressLineOne'
-		}],
-		[{
-			name: 'addressLineTwo',
-			optional: true
-		}],
-		[{
-			name: 'locality'
-		}],
-		[{
-			name: 'county',
-			values: [
-				"Carlow",
-				"Cavan",
-				"Clare",
-				"Cork",
-				"Donegal",
-				"Dublin",
-				"Galway",
-				"Kerry",
-				"Kildare",
-				"Kilkenny",
-				"Laois",
-				"Leitrim",
-				"Limerick",
-				"Longford",
-				"Louth",
-				"Mayo",
-				"Meath",
-				"Monaghan",
-				"Offaly",
-				"Roscommon",
-				"Sligo",
-				"Tipperary",
-				"Waterford",
-				"Westmeath",
-				"Wexford",
-				"Wicklow"
-			]
-		}],
-	],
-	nl: [
-		[{
-			name: 'streetName',
-			example: 'Hoofdstraat'
-		},
-		{
-			name: 'streetNumber',
-			example: '101C'
-		}],
-		[{
-			name: 'zip',
-			example: '1017HG',
-			validator: /^\d{4}\s?\w{2}$/ // 4 digits + [space] + 2 letters, e.g., '1000 PK' or '1000PK' or '1000pk'
-		}]
-	],
-	uk: [
-		[{
-			name: 'addressLineOne'
-		}],
-		[{
-			name: 'addressLineTwo',
-			optional: true
-		}],
-		[{
-			name: 'zip'
-		}]
-	]
+  defaults: defaults,
+  nl: nl,
+  be: nl,
+  uk: uk,
+  ie: ie
 };
---
+++ test/test_all.js
@@ -0,0 +1,187 @@
+"use strict";
+var _ = require('underscore');
+var expect = require('expect.js');
+var address4geo = require('../index.js');
+
+
+
+var countryCodes = ['defaults', 'be', 'ie', 'nl', 'uk'];
+var fieldOptions = {
+    example: {
+        type: typeof ""
+    },
+    optional: {
+        type: typeof true
+    },
+    regexp: {
+        type: typeof /\d+/
+    },
+    values: {
+        type: typeof []
+    }
+};
+
+
+// Make sure we have a parsable package.json
+describe('package.json', function () {
+    var pjson;
+
+    it('should be parsable as JSON', function () {
+        pjson = require('../package.json');
+    });
+    it('should have a name', function () {
+        expect(pjson.name).to.be('address4geo');
+    });
+});
+
+
+describe('all country formats', function () {
+    // Valid list of country-keys (defaut, nl, be, etc)
+    it('should contain known countries', function () {
+        var keys = _.keys(address4geo.formats()).sort();
+        expect(keys).to.eql(countryCodes.sort());
+    });
+
+    // Validate field properties of the listed fields
+    it('should have valid field definitions', function () {
+        _.each(address4geo.formats(), function (country) {
+            expect(country).to.have.property('fields');
+
+            _.each(_.keys(country.fields), function (field) {
+                var fieldDef = country.fields[field];
+                _.each(_.keys(fieldDef), function (opt) {
+                    expect(fieldOptions).to.have.property(opt);
+                    expect(fieldDef[opt]).to.be.an(fieldOptions[opt].type);
+                });
+            });
+        });
+    });
+
+    // Fields mentioned in the presentation should exist in the fields list
+    it('should have valid presentation definitions', function () {
+        _.each(address4geo.formats(), function (country) {
+            expect(country).to.have.property('presentation');
+
+            _.each(country.presentation, function (line) {
+                _.each(line, function (field) {
+                    expect(country.fields).to.have.property(field);
+                })
+            })
+        });
+    });
+});
+
+
+describe('.format()', function () {
+
+    var expectedDefault = {
+        fields: {
+            addressLineOne: {},
+            addressLineTwo: {},
+            locality: {},
+            state: {},
+            zip: {}
+        },
+        presentation: [
+            [ 'addressLineOne' ],
+            [ 'addressLineTwo' ],
+            [ 'locality' ],
+            [ 'state', 'zip' ]
+        ]
+    };
+    it('should return default format when no country is passed', function () {
+        expect(address4geo.format()).to.eql(expectedDefault);
+    });
+    it('should return default format when unknown country is passed', function () {
+        expect(address4geo.format('blahblah')).to.eql(expectedDefault);
+    });
+
+    it("should return 'nl' structure when 'nl' is passed", function () {
+        var expected = {
+              "fields": {
+                  "streetName": {
+                      "example": "Hoofdstraat"
+                  },
+                  "streetNumber": {
+                    "example": "101C"
+                  },
+                  "zip": {
+                      "example": "1017HG",
+                      "regexp": {}
+                  }
+              },
+              "presentation": [
+                  ["streetName", "streetNumber"],
+                  ["zip"]
+            ]
+        };
+        expect(address4geo.format('nl')).to.eql(expected);
+    });
+});
+
+
+describe('.validate()', function () {
+
+    it('should return [] when passed valid data', function () {
+        var validData = {
+            streetName: 'Blahblahgracht',
+            streetNumber: '123C',
+            zip: '1111AA',
+        };
+        var result = address4geo.validate('nl', validData);
+        expect(result).to.eql([]);
+    });
+
+    it("should return [{zip: 'Missing'}] when zip is missing", function () {
+        var invalidData = {
+            streetName: 'Blahblahgracht',
+            streetNumber: '123C'
+        };
+        var result = address4geo.validate('nl', invalidData);
+        expect(result).to.eql([{zip: 'Missing'}]);
+    });
+
+    it("should return [{zip: 'Regexp fail'}] when regexp matching fails", function () {
+        var invalidData = {
+            streetName: 'Blahblahgracht',
+            streetNumber: '123C',
+            zip: 'AA1111'
+        };
+        var result = address4geo.validate('nl', invalidData);
+        expect(result).to.eql([{zip: 'Regexp fail'}]);
+    });
+
+    it("should return [{county: 'Invalid value'}] when unknown county is passed", function () {
+        var invalidCounty = {
+            addressLineOne: 'Blahblahgracht 123C',
+            locality: 'locality',
+            zip: 'zip',
+            county: 'unknown'
+        };
+        var result = address4geo.validate('ie', invalidCounty);
+        expect(result).to.eql([{county: 'Invalid value'}]);
+    });
+
+});
+
+describe('.isValid()', function () {
+    it('should return true when data is fine', function () {
+        var validData = {
+            streetName: 'Blahblahgracht',
+            streetNumber: '123C',
+            zip: '1111AA',
+        };
+        var result = address4geo.isValid('nl', validData);
+        expect(result).to.eql(true);
+    });
+
+
+    it("should return false on invalid data", function () {
+        var invalidData = {
+            streetName: 'Blahblahgracht',
+            streetNumber: '123C'
+        };
+        var result = address4geo.isValid('nl', invalidData);
+        expect(result).to.eql(false);
+    });
+});
